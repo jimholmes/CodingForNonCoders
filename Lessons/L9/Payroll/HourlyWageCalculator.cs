f8d310848879cabf202beafc0ed963172d761fb0
Jim Holmes <jim@GuidepostSystems.com>
2018-12-05 10:54:17 -0800

    Revert L9 to state for Part 1.

--- Lessons/L9/Payroll/HourlyWageCalculator.cs
+++ Lessons/L9/Payroll/HourlyWageCalculator.cs
@@ -6,15 +6,7 @@ namespace L9.Payroll
     {
         public decimal ComputeWages(int hoursWorked, int hourlyRate)
         {
-            if (hoursWorked > 40)
-            {
-                int otHours = hoursWorked - 40;
-                decimal otWages = otHours * hourlyRate * 1.5m;
-                decimal wages = otWages + (40 * hourlyRate);
-                return wages;
-            } else {
-                return hoursWorked * hourlyRate;
-            }
+            return hoursWorked * hourlyRate;
         }
     }
 }
